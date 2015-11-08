01a53f0f37f1c87faef0a1b979e197707a1b2c36
k501 <komai19850822@gmail.com>
2015-11-09 01:28:03 +0900

    make test data

--- rossmann/SQL/mudai.sql
+++ rossmann/SQL/mudai.sql
@@ -20,13 +20,13 @@ select * from tmp_ts_store_state limit 500;
 select * from tmp_submission_20151103_1;
 --train + weather data 
 select * from train_weather limit 1000;
+select * from test_weather limit 1000;
 --train + weather data + sales average by store and day_of_week
 select * from train_weather_avg_op1 limit 1000;
+select * from test_weather_avg_op1 limit 1000;
 
 
-
-
---csvファイル出力
+--csvファイル出力（train_20151109_1）
 select 
     'sales',
     'avg_sales', 'customers','store', 'logyear','logmonth', 'day_of_week', 'competition', 'open',
@@ -38,114 +38,6 @@ select * from train_weather_avg_op1
 INTO OUTFILE '/Users/Komai/OneDrive/tortoise/rossmann/R/data/train_20151109_1.csv' FIELDS TERMINATED BY ','
 --limit 1000
 ;
-
-
-
-
-
-
---天気データに平均を付与
-insert into train_weather_avg_op1
-select
-    aaa.sales,
-    bbb.avg_sales,
-    aaa.customers,
-    aaa.store,
-    aaa.logyear,
-    aaa.logmonth,
-    aaa.day_of_week,
-    aaa.competition,
-    aaa.open,
-    aaa.state_holiday,
-    aaa.school_holiday,
-    aaa.promo,
-    aaa.promo2,
-    aaa.store_type,
-    aaa.assortment,
-    aaa.state,
-    aaa.logdate,
-    aaa.rain,
-    aaa.fog,
-    aaa.snow,
-    aaa.hail,
-    aaa.thunder
-from (
-    select * from train_weather 
-) aaa
-left outer join (
-    select 
-        store, day_of_week, avg(sales) avg_sales 
-    from train 
-    where open = 1
-    group by store, day_of_week
-) bbb
-on aaa.store = bbb.store and aaa.day_of_week = bbb.day_of_week
---limit 1000
-;
-
-
-
-
-
-
-
-
---csvファイル出力
---select 
---    'sales','customers','store', 'logyear','logmonth', 'day_of_week', 'competition', 'open',
---    'state_holiday', 	'school_holiday','promo','promo2','store_type',	'assortment',
---    'state', 'logdate', 'rain', 'fog', 'snow', 'hail', 'thunder'
---from dual 
---union --ヘッダーを出力するとき、こーやる
---insert into train_weather
-select 
-    aaa.*,
-    bbb.rain,
-    bbb.fog,
-    bbb.snow,
-    bbb.hail,
-    bbb.thunder
-from (
-    select * from tmp_tr_store_state 
-    where open = 1
-) aaa
-left outer join (
-    select 
-        state,
-        logdate,
-        case 
-            when events = '' then '1'
-            else '0'
-        end sunny,
-        case
-            when events like '%Rain%' then 1
-            else '0'
-        end rain,
-        case
-            when events like '%Fog%' then 1
-            else '0'
-        end fog,
-        case
-            when events like '%Snow%' then 1
-            else '0'
-        end snow,
-        case
-            when events like '%Hail%' then 1
-            else '0'
-        end hail,
-        case
-            when events like '%Thunderstorm%' then 1
-            else '0'
-        end thunder
-    from weather
-) bbb
-on aaa.state = bbb.state and aaa.logdate = bbb.logdate
-limit 1500
---INTO OUTFILE '/Users/Komai/OneDrive/kaggle/rossmann/R/data/train_20151103_1.csv' FIELDS TERMINATED BY ','
-;
-
-
-
 
 
 
