9a25c7a935ee9386b5ea17d4424e0158d9ef721d
k501 <komai19850822@gmail.com>
2015-11-24 03:06:56 +0900

    convert categorical to dummy

--- prudential/R/import.R
+++ prudential/R/import.R
@@ -1,6 +1,18 @@
+install.packages("caret")
 
+library(caret)
+library(ggplot2)
 
 train <- read.csv("data//train.csv", header=T)
+
+# define categorical variables
+train$Product_Info_1 <- as.factor(train$Product_Info_1)
+train$Product_Info_2 <- as.factor(train$Product_Info_2)
+train$Product_Info_3 <- as.factor(train$Product_Info_3)
+
+# convert categorical variables to dummy
+train.tmp <- dummyVars(~., data=train)
+train.dummy <- as.data.frame(predict(train.tmp, train))
 
 
 
