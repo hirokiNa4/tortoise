a7ea09a3645ba2facf3a08a370292015c33447f1
k501 <komai19850822@gmail.com>
2015-11-30 23:57:25 +0900

    at last, he works

--- prudential/python/main/train.py
+++ prudential/python/main/train.py
@@ -19,11 +19,12 @@ def train_yaml(yaml_file):
 def train_layer1(yaml_file_path, save_path):
 
     yaml = open("{0}/dae_l1.yaml".format(yaml_file_path), 'r').read()
-    hyper_params = {'train_stop': 50,
+    hyper_params = {'train_stop': 1000,
                     'batch_size': 50,
                     'monitoring_batches': 1,
-                    'nhid': 10,
-                    'max_epochs': 1,
+                    'nvis': 900,
+                    'nhid': 50,
+                    'max_epochs': 100,
                     'save_path': save_path}
     yaml = yaml % (hyper_params)
     train_yaml(yaml)
@@ -32,12 +33,12 @@ def train_layer1(yaml_file_path, save_path):
 def train_layer2(yaml_file_path, save_path):
 
     yaml = open("{0}/dae_l2.yaml".format(yaml_file_path), 'r').read()
-    hyper_params = {'train_stop': 50,
+    hyper_params = {'train_stop': 1000,
                     'batch_size': 50,
                     'monitoring_batches': 1,
-                    'nvis': 10,
-                    'nhid': 10,
-                    'max_epochs': 1,
+                    'nvis': 50,
+                    'nhid': 50,
+                    'max_epochs': 100,
                     'save_path': save_path}
     yaml = yaml % (hyper_params)
     train_yaml(yaml)
@@ -46,10 +47,12 @@ def train_layer2(yaml_file_path, save_path):
 def train_mlp(yaml_file_path, save_path):
 
     yaml = open("{0}/dae_mlp.yaml".format(yaml_file_path), 'r').read()
-    hyper_params = {'train_stop': 50,
-                    'valid_stop': 50050,
+    hyper_params = {'train_stop': 1000,
+                    'valid_stop': 1000,
                     'batch_size': 50,
-                    'max_epochs': 1,
+                    'max_epochs': 100,
+                    'nvis': 900,
+                    'n_class': 9,
                     'save_path': save_path}
     yaml = yaml % (hyper_params)
     train_yaml(yaml)
@@ -59,11 +62,15 @@ def test_sda():
 
     skip.skip_if_no_data()
 
-    yaml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    save_path = os.path.dirname(os.path.realpath(__file__))
+    yaml_file_path = '..';
+    save_path = '.'
 
+
+    print '=== train_layer1 =========================================================='
     train_layer1(yaml_file_path, save_path)
+    print '=== train_layer2 =========================================================='
     train_layer2(yaml_file_path, save_path)
+    print '=== fine tuning ==========================================================='
     train_mlp(yaml_file_path, save_path)
 
     try:
